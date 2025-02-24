e8ccd11cf4b40d8e3582ea448d213b92b063807b
iceprosurface <775846939@qq.com>
2025-02-24 18:13:21 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Dialog from './Dialog.vue';
+const show = ref(false);
+function showPopup() {
+  show.value = !show.value;
+}
+</script>
+
+<template>
+  <div>
+    <button @click="showPopup">显示弹窗</button>
+    <Dialog v-model:show="show"></Dialog>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/Dialog.vue
@@ -0,0 +1,115 @@
+<template>
+  <div
+    class="dialog"
+    :class="{
+      dialogShow: show,
+    }"
+  >
+    <div
+      class="dialogBody"
+      ref="target"
+      :style="{
+        transform: onDraging
+          ? `translateY(${y - startY > 0 ? y - startY : 0}px)`
+          : undefined,
+        transition: onDraging ? 'unset' : undefined,
+      }"
+    >
+      <div class="dialogDrag" @mousedown="startDrag">拖拽条</div>
+      <HelloWorld />
+    </div>
+  </div>
+</template>
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import { ref, useTemplateRef } from 'vue';
+import {
+  useEventListener,
+  onClickOutside,
+  useMouse,
+  useElementSize,
+} from '@vueuse/core';
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+  },
+});
+const target = useTemplateRef('target');
+const emit = defineEmits(['update:show']);
+function close() {
+  emit('update:show', false);
+}
+onClickOutside(target, (event) => {
+  close();
+});
+const { x, y } = useMouse();
+const onDraging = ref(false);
+const { height } = useElementSize(target);
+let startY = 0;
+function startDrag(event) {
+  onDraging.value = true;
+  startY = y.value;
+  console.log('start drag');
+}
+function draging() {
+  if (onDraging.value) {
+    console.log(y.value);
+  }
+}
+function dragEnd(event) {
+  if (onDraging.value) {
+    event.stopPropagation();
+    event.preventDefault();
+    console.log(event);
+  }
+  if (y.value - startY > height.value * 0.5) {
+    emit('update:show', false);
+  }
+  onDraging.value = false;
+  console.log('end drag');
+  return false;
+}
+useEventListener(document.body, 'mousemove', draging);
+useEventListener(document.body, 'mouseup', dragEnd);
+</script>
+
+<style>
+.dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  transition: opacity 0.3s ease-in-out;
+  opacity: 0;
+  pointer-events: none;
+}
+.dialogShow {
+  opacity: 1;
+  pointer-events: unset;
+}
+.dialogShow .dialogBody {
+  transform: translateY(0%);
+}
+
+.dialogBody {
+  height: 40vh;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: #fff;
+  padding: 32px;
+  transform: translateY(100%);
+  transition: transform 0.3s ease-in-out;
+}
+.dialogDrag {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  user-select: none;
+}
+</style>
